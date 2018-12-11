e0cfe67010c77e535bb3b8b06f28aa184ce342f9
izumisan <izumi.mite@gmail.com>
2018-12-11 22:27:43 +0900

    update observable property sample

--- samples/ObservableProperty/main.qml
+++ samples/ObservableProperty/main.qml
@@ -6,8 +6,8 @@ import viewmodels 1.0
 
 Window {
     visible: true
-    width: 640
-    height: 480
+    width: 400
+    height: 350
     title: qsTr("Observable Property Sample")
 
     MainViewModel { id: vm }
@@ -19,18 +19,45 @@ Window {
         Label {
             text: vm.ivalue.value
             font.pixelSize: 16
+            Layout.minimumWidth: 50
         }
         SpinBox {
+            value: vm.ivalue.value
             Layout.fillWidth: true
+
             onValueChanged: vm.ivalue.value = value
         }
 
-        Label {  }
-        SpinBox {
-            Layout.fillWidth: true
+
+        Label {
+            text: vm.fooName.value
+            font.pixelSize: 16
+        }
+        TextField {
+            text: vm.fooName.value
+
+            onTextEdited: vm.fooName.value = text
         }
 
-        Label { }
-        TextField { }
+        Label {
+            text: vm.fooValue.value
+            font.pixelSize: 16
+        }
+        SpinBox {
+            value: vm.fooValue.value
+            Layout.fillWidth: true
+
+            onValueChanged: vm.fooValue.value = value
+        }
+    }
+
+    Button {
+        text: "print foo"
+        anchors.bottom: parent.bottom
+        anchors.right: parent.right
+        anchors.bottomMargin: 20
+        anchors.rightMargin: 20
+
+        onClicked: vm.printFooValue()
     }
 }
--- samples/ObservableProperty/mainviewmodel.h
+++ samples/ObservableProperty/mainviewmodel.h
@@ -1,28 +1,52 @@
 #ifndef MAINVIEWMODEL_H
 #define MAINVIEWMODEL_H
 
+#include <memory>
 #include <QObject>
+#include <QVariant>
+#include <QDebug>
 #include "observableproperty.h"
+#include "foo.h"
 
 class MainViewModel : public QObject
 {
     Q_OBJECT
     Q_PROPERTY( izm::qmvvm::ObservablePropertyBase* ivalue READ ivalue CONSTANT )
+    Q_PROPERTY( izm::qmvvm::ObservablePropertyBase* fooName READ fooName CONSTANT )
+    Q_PROPERTY( izm::qmvvm::ObservablePropertyBase* fooValue READ fooValue CONSTANT )
 
 public:
     MainViewModel( QObject* parent = nullptr )
         : m_ivalue( izm::qmvvm::ObservableHelper<int>::getter(),
                     izm::qmvvm::ObservableHelper<int>::setter(),
                     parent )
+        , m_fooName( [](const std::shared_ptr<Foo>& data) { return data->name(); },
+                     [](std::shared_ptr<Foo>& data, const QVariant& value) { data->setName( value.toString() ); } )
+        , m_fooValue( [](const std::shared_ptr<Foo>& data) { return data->value(); },
+                      [](std::shared_ptr<Foo>& data, const QVariant& value) { data->setValue( value.toInt() ); } )
     {
+        m_fooName.setValue( m_foo );
+        m_fooValue.setValue( m_foo );
     }
     virtual ~MainViewModel() = default;
 
 public:
     izm::qmvvm::ObservableProperty<int>* ivalue() { return &m_ivalue; }
+    izm::qmvvm::ObservableProperty<std::shared_ptr<Foo>>* fooName() { return &m_fooName; }
+    izm::qmvvm::ObservableProperty<std::shared_ptr<Foo>>* fooValue() { return &m_fooValue; }
+
+public slots:
+    void printFooValue() const
+    {
+        qDebug() << m_foo->name() << m_foo->value();
+    }
 
 private:
     izm::qmvvm::ObservableProperty<int> m_ivalue;
+    izm::qmvvm::ObservableProperty<std::shared_ptr<Foo>> m_fooName = nullptr;
+    izm::qmvvm::ObservableProperty<std::shared_ptr<Foo>> m_fooValue = nullptr;
+
+    std::shared_ptr<Foo> m_foo = std::make_shared<Foo>( "foo", 0 );
 };
 
 #endif // MAINVIEWMODEL_H
