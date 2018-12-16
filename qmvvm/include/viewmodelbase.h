49f835b4a3e169efb1bb64f3ec7b6ba38eb92bc9
izumisan <izumi.mite@gmail.com>
2018-12-16 23:01:35 +0900

    add viewmodelbase

---
+++ qmvvm/include/viewmodelbase.h
@@ -0,0 +1,52 @@
+#ifndef IZM_QMVVM_VIWEMODELBASE_H
+#define IZM_QMVVM_VIWEMODELBASE_H
+
+#include <functional>
+#include <QObject>
+#include "qmvvm_global.h"
+#include "observableproperty.h"
+
+#define OBSERVABLE_PROPERTY( name, var ) \
+    Q_PROPERTY( izm::qmvvm::ObservablePropertyBase* name READ get_##var CONSTANT ) \
+    izm::qmvvm::ObservablePropertyBase* get_##var() { return &( var ); }
+
+#define COMMAND_PROPERTY( name, var ) \
+    Q_PROPERTY( izm::qmvvm::CommandBase* name READ get_##var CONSTANT ) \
+    izm::qmvvm::CommandBase* get_##var() { return &( var ); }
+
+namespace izm
+{
+namespace qmvvm
+{
+
+class IZMQMVVMSHARED_EXPORT ViewModelBase : public QObject
+{
+    Q_OBJECT
+public:
+    explicit ViewModelBase( QObject* parent = nullptr );
+    virtual ~ViewModelBase() = default;
+
+    template<class T>
+    bool setProperty( ObservableProperty<T>& property,
+                      const T& value,
+                      const std::function<void()>& onChanged = nullptr )
+    {
+        bool ret = false;
+        auto&& saved = property.value();
+        property.setValue( value );
+        if ( saved != property.value() )
+        {
+            ret = true;
+            if ( onChanged )
+            {
+                onChanged();
+            }
+        }
+        return ret;
+    }
+};
+
+} // namespace qmvvm
+} // namespace izm
+
+#endif // IZM_QMVVM_VIWEMODELBASE_H
